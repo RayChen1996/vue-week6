e49f94c3c781418609ae4726bb63583edb54c4f6
RayChen1996 <53119155+RayChen1996@users.noreply.github.com>
2023-02-20 06:36:37 +0800

    修改config

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: "/Vue-week6/",
+  base: "/vue-week6/",
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
